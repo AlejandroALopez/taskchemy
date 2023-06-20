630dee74f07bf8e0fe49444adb69e099684061de
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-06-20 15:05:52 -0400

    task components

---
+++ components/tasks/TaskItem.js
@@ -0,0 +1,16 @@
+
+
+function TaskItem(props) {
+
+  return (
+    <li className={""}>
+      <div>
+        <div className={""}>
+            <p className={""}>{props.title}</p>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default TaskItem;
