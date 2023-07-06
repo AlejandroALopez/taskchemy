de7683c15c71312fb165d77409583814e4208975
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-07-06 15:05:01 -0400

    no nav bar on login and signup pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,24 @@ import { SessionProvider } from "next-auth/react";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
+  ...appProps
 }: AppProps) {
+  const noLayoutPages = [`/login`, `/signup`];
+  const getContent = () => {
+    if (noLayoutPages.includes(appProps.router.pathname)) {
+      return <Component {...pageProps} />;
+    } else {
+      return (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      );
+    }
+  };
+
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {getContent()}
     </SessionProvider>
   );
 }
